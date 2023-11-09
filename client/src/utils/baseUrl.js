7906fe4089ca14ab41de0464706edc9c8debfa14
Midlaj2000 <midlajmuhammed619@gmail.com>
2023-11-10 00:31:48 +0530

    latest commit

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,3 +1,3 @@
 // export const baseUrl = "http://localhost:3000/api"
 
-export const baseUrl = "https://genuine-sopapillas-48d2ab.netlify.app/"
+export const baseUrl = "https://growword.vercel.app/"
